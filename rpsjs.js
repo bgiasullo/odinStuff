6e42510a74e259704f5712c4ed1594f7060a9950
bgiasullo <bgiasullo@gmail.com>
2023-04-29 17:38:19 -0400

    Add comments to plan the solution

--- rpsjs.js
+++ rpsjs.js
@@ -1,3 +1,13 @@
   
-/* Get random choice from computer: rock, paper, or scissor */
+/* Get random choice from computer: rock, paper, or scissors */
 let getComputerChoice 
+
+/* Provide three buttons for the user to select rock, paper, or scissors. */
+
+/* If user's choice equals computer's choice, return message: It's a tie! */
+
+/* If user chooses paper and computer chooses rock, return message: You win! Else return message: You lose! */
+
+/* If user chooses rock and computer chooses scissors, return message: You win! Else return message: You lose! */
+
+/* If user chooses scissors and computer chooses paper, return message: You win! Else return message: You lose! */
