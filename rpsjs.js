af8fce9f4302b7e1018411d3e9f9f1e0cf653b6b
bgiasullo <bgiasullo@gmail.com>
2023-05-09 20:33:03 -0400

    Start over

--- rpsjs.js
+++ rpsjs.js
@@ -3,40 +3,12 @@
 
 function computerChoice(choices)
 {
-  
-return choices[Math.floor(Math.random()*choices.length)];
-     
-}
+    return choices[Math.floor(Math.random()*choices.length)];
+}  
 
 const choices = ['Roock', 'Pooper', 'Scoosers'];
-console.log(random_item(choices));
 
 
-function roock() {
-    document.getElementById("youChose").innerHTML = "You chose roock!";
-  }
-
-  if (computerSelection = "Pooper") {
-    document.getElementById("results").innerHTML = "Pooper beats roock! You lose. :(";
-  } else {
-    document.getElementById("results").innerHTML = "Roock beats scoosers! You win! :)";
-}
-
-function pooper() {
-    document.getElementById("youChose").innerHTML = "You chose pooper!";
-  }
-
-function scoosers() {
-    document.getElementById("youChose").innerHTML = "You chose scoosers!";
-  }
-
-/* Provide three buttons for the user to select rock, paper, or scissors. */
-
-/* If user's choice equals computer's choice, return message: Computer chose CHOICE It's a tie! */
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
 
 /* If user chooses paper and computer chooses rock, return message: Computer chose CHOICE. You win! Else return message: You lose! */
 
