3b55f94692854676d689d7a89da90371f0779ec7
bgiasullo <bgiasullo@gmail.com>
2023-05-15 21:00:12 -0400

    Clear the results function to start over next time

--- rpsjs.js
+++ rpsjs.js
@@ -3,18 +3,26 @@
 
 
 const choices = ['Roock', 'Pooper', 'Scoosers'];
+
 console.log(computerChoice(choices));
 
-function computerChoice(choices)
-{
+function computerChoice(choices) {
     return choices[Math.floor(Math.random()*choices.length)];
 }  
 
-function seeComputer()
-{
-    document.getElementById("computerChose").innerHTML = "Computer has chosen.";
-    document.getElementById("nowYou").innerHTML = "Now you choose...";
-} 
+function roock() {
+    document.getElementById("computerChose").innerHTML = "Computer chose " + computerChoice(choices);
+    document.getElementById("youChose").innerHTML = "You chose Roock";
+}
+
+
+if (computerChoice(choices) === 'Roock') {
+    document.getElementById('results').innerHTML = "It's a tie. Foo!";
+} else if (computerChoice(choices) === 'Pooper') {
+    document.getElementById('results').innerHTML = "Pooper boots roock. You lose.";
+} else {
+    document.getElementById('results').innerHTML = "Roock boops Scoosers. You win!";
+}
 
 
 /* If user chooses paper and computer chooses rock, return message: Computer chose CHOICE. You win! Else return message: You lose! */
