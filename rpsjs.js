8d6bd22a3b4e05a03c4a434f5fea6afb4f1f1ec5
bgiasullo <bgiasullo@gmail.com>
2023-05-21 10:48:19 -0400

    Mess everything up before starting over again.

--- rpsjs.js
+++ rpsjs.js
@@ -4,25 +4,29 @@
 
 const choices = ['Roock', 'Pooper', 'Scoosers'];
 
-console.log(computerChoice(choices));
 
-function computerChoice(choices) {
+
+let computerChoice = function getComputerCh(choices) {
     return choices[Math.floor(Math.random()*choices.length)];
 }  
 
-function roock() {
-    document.getElementById("computerChose").innerHTML = "Computer chose " + computerChoice(choices);
-    document.getElementById("youChose").innerHTML = "You chose Roock";
+console.log(computerChoice);
+
+function getPlayerCh() {
+    let playerChoice 
+    document.getElementById("youChose").innerHTML = "You chose " + playerChoice; 
+    document.getElementById("computerChose").innerHTML = "Computer chose " + computerChoice;
 }
 
-
-if (computerChoice(choices) === 'Roock') {
+if (computerChoice === playerChoice) {
     document.getElementById('results').innerHTML = "It's a tie. Foo!";
-} else if (computerChoice(choices) === 'Pooper') {
+} else if (computerChoice === 'Pooper' && ) {
     document.getElementById('results').innerHTML = "Pooper boots roock. You lose.";
 } else {
     document.getElementById('results').innerHTML = "Roock boops Scoosers. You win!";
-}
+}  
+
+
 
 
 /* If user chooses paper and computer chooses rock, return message: Computer chose CHOICE. You win! Else return message: You lose! */
